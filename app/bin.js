637ed69169e133024c6a09e47a2ff8f52ec8309d
slashhuang <627284482@qq.com>
2017-02-13 14:25:24 +0800

    add pm2 and app/index

---
+++ app/bin.js
@@ -0,0 +1,15 @@
+/*
+ * built by slashhuang
+ * 2017.2.13
+ * 主程序入口
+ */
+
+ const http = require('http');
+
+const PORT = 3000;
+
+ http.createServer((req,res)=>{
+    res.write('hello world');
+    res.end('response stream done')
+ }).listen(PORT);
+ console.log(`app starting on port ${PORT}`)
---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ * built by slashhuang
+ * 2017.2.13
+ */
+
+ require('babel-register');
+ require('app/bin');
